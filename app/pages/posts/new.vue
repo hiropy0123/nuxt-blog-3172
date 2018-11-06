32941cd764aa9eeba64fe2e109702841263bd7bc
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-11-06 23:54:23 +0900

    upgrade package

---
+++ app/pages/posts/new.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container posts-page">
+    <el-card style="flex: 1;">
+      <div slot="header" class="clearfix">
+        
+      </div>
+    </el-card>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
